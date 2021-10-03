4d732f4a7fa43092bf5640f2c936a490c6add68d
Xin Wei <weixinary@hotmail.com>
2021-10-03 16:37:07 -0400

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -7,10 +7,6 @@ Created on Fri Oct  1 15:00:58 2021
 
 # This code is used to create 'score.csv' from the original 'SCORE_csv.xlsx' file.
 
-# to load xlsx, need to 'pip install xlrd'
-#Successfully installed xlrd-2.0.1
-#XLRDError: Excel xlsx file; not supported
-#not working
 
 #pip uninstall xlrd
 #pip install xlrd==1.2.0
@@ -24,6 +20,6 @@ from flair.data import Sentence
 
 
 path = r'SCORE_csv.xlsx'
-claims = pd.read_excel(path, sheet_name = 0, usecols = ['DOI_CR', 'paper_id', 'coded_claim2','coded_claim3a','coded_claim3b', 'coded_claim4'], header=0)  #usecols = [0, 1]:choose column 5 and 9    #header=0: 
+claims = pd.read_excel(path, sheet_name = 0, usecols = ['DOI_CR', 'paper_id', 'coded_claim2','coded_claim3a','coded_claim3b', 'coded_claim4'], header=0)  
 
 claims.to_csv('score.csv', encoding='utf-8', index=0)
